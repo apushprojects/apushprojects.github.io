5bcb98ce7c4e51679e8307522f10cdb1a18557cc
Shivam Agrawal <shivamlego@gmail.com>
2015-11-08 12:04:35 -0500

    initial commit

---
+++ libs/main.js
@@ -0,0 +1,72 @@
+$(function() {   
+	$("body").fadeIn(1000, function() {
+		$("#market-rev-intro").owlCarousel({
+			slideSpeed : 300,
+			paginationSpeed : 400,
+			singleItem : true,
+			autoPlay : 3000,
+			stopOnHover : false,
+			transitionStyle : 'fade',
+			mouseDrag: false,
+			touchDrag: false
+		});
+		$("#market-rev-intro").css('visibility', 'visible');
+		$("#market-rev-intro").addClass('animated fadeIn');
+		var repeater = setInterval(scrollCheck, 50);
+	}); 
+	// reason 1 interaction
+	$("#map1").popover({ container: ".mapContainer", placement: "right" });
+	$("#map2").popover({ container: ".mapContainer", placement: "left" });
+	$("#map3").popover({ container: ".mapContainer", placement: "right" });
+	// reason 2 interaction
+	$("#spinning-wheel-compare").on('touchstart click', function(event) {
+		if ($(this).attr("src") != "imgs/reason2/spinning-wheel-compare-2.jpg") {
+			$(this).fadeOut(400, function() {
+				$(this).attr("src", "imgs/reason2/spinning-wheel-compare-2.jpg");
+			}).fadeIn(400);
+		} else {
+			$(this).fadeOut(400, function() {
+				$(this).attr("src", "imgs/reason2/spinning-wheel-compare-1.png");
+			}).fadeIn(400);
+		}
+	});
+	$("#cotton-gin-compare").on('touchstart click', function(event) {
+		if ($(this).attr("src") != "imgs/reason2/cotton-gin-compare-2.png") {
+			$(this).fadeOut(400, function() {
+				$(this).attr("src", "imgs/reason2/cotton-gin-compare-2.png");
+			}).fadeIn(400);
+		} else {
+			$(this).fadeOut(400, function() {
+				$(this).attr("src", "imgs/reason2/cotton-gin-compare-1.png");
+			}).fadeIn(400);
+		}
+	});
+	// smooth scroll
+	$('a[href*=#]:not([href=#])').on('touchstart click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top - 84
+				}, 1000);
+				return false;
+			}
+		}
+	});
+});
+var intro = $("#intro"), reason1 = $("#reason1"), reason2 = $("#reason2"), outcomes = $("#outcomes");
+function scrollCheck() {
+	if (isScrolledIntoView(intro)) { intro.css('visibility', 'visible'); intro.addClass('animated bounceInLeft'); }
+	if (isScrolledIntoView(reason1)) { reason1.css('visibility', 'visible'); reason1.addClass('animated bounceInRight'); }
+	if (isScrolledIntoView(reason2)) { reason2.css('visibility', 'visible'); reason2.addClass('animated bounceInLeft'); }
+	if (isScrolledIntoView(outcomes)) { outcomes.css('visibility', 'visible'); outcomes.addClass('animated bounceInRight'); scrollDone(); }
+}
+function scrollDone() { clearInterval(repeater); }
+function isScrolledIntoView(elem) {
+	var $window = $(window);
+	var docViewTop = $window.scrollTop();
+	var docViewBottom = docViewTop + $window.height();
+	var elemTop = elem.offset().top;
+	return (elemTop <= docViewBottom);
+}
